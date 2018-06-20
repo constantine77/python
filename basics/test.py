1e7fc3a4260156ad1317e6ba1f2c83d61dc8b3f8
admin <admin@admins-MacBook-Pro.local>
2018-06-20 00:51:58 -0500

    added list logic

--- basics/test.py
+++ basics/test.py
@@ -1,11 +1,36 @@
 
-#Data structures in Python
+#Data Structures in Python
 
-def simple_list():
+def simple_list_iteration():
+    '''
+    List iteration
+    :return:
+    '''
     A = [1,2,3,4,5]
     for x in A:
         print(x,type(x))
+        x += 1
+        print(x)
 
+def list_index_iteration():
+    '''
+    List iteration with index
+    :return:
+    '''
+    A = [1,2,3,4,5]
+    for k in range(5): #from o to 4, k is index
+        print(A[k])
 
-simple_list()
+def create_list():
+    '''
+    create List with 10 elements
+    :return:
+    '''
+    A=[0]*10
+    top = 0
+    x = int(input())
+    while x != 0:
+        A[top]=x
+    print(A)
 
+create_list()
